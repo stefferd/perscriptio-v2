b02f494c44b13affb30e0099e648228b85003ee3
stefferd <stef.van.den.berg@philips.com>
2016-06-21 15:56:00 +0200

    Initial checkin first version

---
+++ app.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import { bootstrap } from "@angular/platform-browser-dynamic";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'perscriptio',
+  template: `
+    <div>
+      perscriptio
+    </div>`
+})
+class Perscriptio {
+}
+
+bootstrap(Perscriptio);
+
